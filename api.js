a83b44a7930844e449f09f8e11d1501c4a293f80
Hieu <pmh.ars14@gmail.com>
2018-07-18 08:49:14 +0700

    applied api

--- api.js
+++ api.js
@@ -1,98 +1,37 @@
 var express = require('express');
 var app = express();
 var dao = require('./dao');
-var mysql = require('mysql');
+
 var config = require('./config');
 
-var conn = mysql.createConnection(config.dbConfig);
+app.get('/getStudent', function (req, res) {
+    let rollNumber = req.query.rollNumber;
+    let name = req.query.name;
+    var student;
 
-const PORT = config.port;
+    var callbackDao = function (err, result) {
+    	if (err) {
+    		console.log(err);
+    		return;
+    	}
+    	let _result = {
+    		success: 1,
+    		students: result
+    	}
+    	res.end(JSON.stringify(_result));
+    }
 
-app.get('/getStudent', function(req,res) {
-    var rollNumber = req.query.rollNumber;
-	var name = req.query.name;
+    if (rollNumber) {
+    	dao.getStudentByRollNumber(rollNumber.trim(), callbackDao);
+    }
 
-	if (rollNumber) {
-		getStudentByRollNumber(rollNumber.trim(), res);
-		return;
-	}
-	if (name) {
-		getStudentsByName(name.trim(), res);
-		return;
-	}
+    if (name) {
+    	dao.getStudentsByName(name.trim(), callbackDao);
+    }
 });
 
-var server = app.listen(PORT, function () {
-	// var host = server.address().address;
+var server = app.listen(8081, function () {
 	let host = config.dbConfig.host;
-	console.log('API server is listening at http://%s:%s', host, PORT);
+	let port = config.port;
+	console.log('API server is listening at http://%s:%s...', host, port);
 });
-
-function getStudentByRollNumber (rollNumber, res) {
-	var students;
-	let queryString = "SELECT * FROM `student` WHERE rollNumber = '" + rollNumber + "'";
-	conn.query(queryString, function (err, result, fields) {
-		if (err || !result[0]) {
-			// let data = { success: 0 }
-			students = null;
-		}
-		students = [ result[0] ];
-		let _result = { success: 0 };
-		if (!students) {
-			res.end(JSON.stringify(_result));
-		}
-
-		_result = {
-			success: 1,
-			students: students
-		}
-		res.end(JSON.stringify(_result));
-	});
-}
-
-function getStudentsByName (name, res) {
-	let queryString = "SELECT * FROM `student` WHERE fullName like '%" + name + "%'";
-	conn.query(queryString, function (err, result, fields) {
-		if (err) { }
-
-		let _result = { success: 0 };
-
-		if (!result || result.length === 0) {
-			res.end(JSON.stringify(_result));
-		}
-
-		_result = {
-			success: 1,
-			students: result
-		}
-		res.end(JSON.stringify(_result));
-	});
-}
-
-// app.get('/getStudent', function (req, res) {
-// 	var rollNumber = req.query.rollNumber;
-// 	var name = req.query.name;
-
-// 	if (rollNumber) {
-// 		var student;
-// 		let queryString = "SELECT * FROM `student` WHERE rollNumber = '" + rollNumber + "'";
-// 		conn.query(queryString, function (err, result, fields) {
-// 			if (err || !result[0]) {
-// 				// let data = { success: 0 }
-// 				student = null;
-// 			}
-// 			student = result[0];
-// 			if (!student) {
-// 				let _result = { success: 0 };
-// 				res.end(JSON.stringify(_result));
-// 			}
-
-// 			let _result = {
-// 				success: 1,
-// 				student: student
-// 			}
-// 			res.end(JSON.stringify(_result));
-// 		});
-// 		// let student = dao.getStudentByRollNumber(rollNumber);
-// 	}
-// });
